086c7660840afd81006a6cf80bc515717a3b3017
d-VorNick <70892582+d-VorNick@users.noreply.github.com>
2020-09-21 10:52:47 +0300

    Update script7.sh

--- Lab1/script7.sh
+++ Lab1/script7.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -h -o "[a-zA-Z0-9_\.-]\+@[a-zA-Z0-9]\+\.[a-zA-Z0-9]\+" /etc/* 2> /dev/null | awk '{printf "%s, ",$1}' > emails.lst
+grep -h -o -r "[a-zA-Z0-9_\.-]\+@[a-zA-Z0-9]\+\.[a-zA-Z0-9]\+" /etc/* 2> /dev/null | awk '{printf "%s, ",$1}' > emails.lst
